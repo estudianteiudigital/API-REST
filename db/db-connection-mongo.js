d6b8417023f9460a13bebcf5a1653d12ff4871a8
estudianteiudigital <cristina.sanchez@est.iudigital.edu.co>
2022-10-01 23:53:52 -0500

    nueva modificacion

--- db/db-connection-mongo.js
+++ db/db-connection-mongo.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose');
+  const mongoose = require('mongoose');
 
 // creamos una funcion flecha encargada de realizar la conexion a la base de datos
 
-const getConnection = async () => {  //funcion asincrona
+  const getConnection = async () => {  //funcion asincrona
     try{
 
         const url = 'mongodb://user_bd:3vRY3czeQEkuyAy9@ac-7uambeg-shard-00-00.nqbc21y.mongodb.net:27017,ac-7uambeg-shard-00-01.nqbc21y.mongodb.net:27017,ac-7uambeg-shard-00-02.nqbc21y.mongodb.net:27017/inventario-app?ssl=true&replicaSet=atlas-34tvq0-shard-0&authSource=admin&retryWrites=true&w=majority';
